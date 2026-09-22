62751975e48d79401b612e7d0ac86ade75860313
Md Mushfiqur Talha Khondoker <161893411+talha-khondoker@users.noreply.github.com>
2026-09-22 23:45:14 +0600

    Update database connection URL for Supabase

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Talhakhondoker@db.imedwncstlqixhsfnrdv.supabase.co:6543/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.imedwncstlqixhsfnrdv:Talhakhondoker@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL )
 SessionLocal = sessionmaker(autoflush=False, autocommit= False, bind=engine)
